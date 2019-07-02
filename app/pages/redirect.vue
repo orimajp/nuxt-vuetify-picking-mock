62deb0edcb065b48a4dc8e787b946254f7d68f67
Hideo Oshima <orimajp@gmail.com>
2019-07-02 23:56:24 +0900

    リロード時File Not Foundリダイレクトテスト

---
+++ app/pages/redirect.vue
@@ -0,0 +1,22 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  layout: 'blank',
+  head() {
+    return {
+      title: '',
+    }
+  },
+  fetch({base, redirect, query}) {
+    const param = query.p
+    if (param === undefined) {
+      return redirect('/')
+    }
+    const redirectPath = '/' + param.replace(base, '')
+    return redirect(redirectPath)
+  }
+}
+</script>
